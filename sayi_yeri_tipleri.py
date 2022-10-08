c27ff7a44d770dde2677e46478045d2b58205aa2
emrehanP <114801754+emrehanP@users.noreply.github.com>
2022-10-08 11:26:49 +0300

    Create sayi_yeri_tipleri.py

---
+++ sayi_yeri_tipleri.py
@@ -0,0 +1,28 @@
+"""
+İki tür veri tipi vardır: integer, float (numbers)
+    int: 1, 75
+    float: 8.5, 12.8
+"""
+
+# TAM SAYILARLA İŞLEM YAPALIM
+print(8+7)
+
+# ONDALIK SAYILARLA İŞLEM YAPALIM
+print(1.6*3.7)
+
+# VERİLERİN TİBİNE BAKALIM: type()
+print(type(5))
+print(type (19.7))
+
+print(type (31))
+
+
+
+
+
+
+
+
+
+
+

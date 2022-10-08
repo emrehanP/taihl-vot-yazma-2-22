29e838427408d647e96163c5522204a1e6bf07c1
emrehanP <114801754+emrehanP@users.noreply.github.com>
2022-10-08 11:55:44 +0300

    matematiksel sayı veri tipleri öğrenildi

---
+++ matematiksel_operatörler.py
@@ -0,0 +1,77 @@
+"""
+Toplama: +
+Çıkarma: -
+Çarpma: *
+Bölme: /
+Üs: **
+Mod: %
+Tam Bölme: //
+"""
+
+#TOPLAMA
+print(5487+9241)
+
+#ÇIKARMA
+print(3-1)
+
+#ÇARPMA
+print(1*3)
+
+
+
+#BÖLME
+print(3/2)
+
+#ÜS ALMA
+print(792634**123)
+
+
+#MOD ALMA
+print(40%3)
+
+#TAM BÖLME
+print(40//9)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

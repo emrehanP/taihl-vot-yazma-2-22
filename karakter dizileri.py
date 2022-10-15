2d32880b0de7847d903d0fe04ad25d5a687d94f4
emrehanP <114801754+emrehanP@users.noreply.github.com>
2022-10-15 11:32:10 +0300

    Create karakter dizileri.py

---
+++ karakter dizileri.py
@@ -0,0 +1,17 @@
+# Kullanıcı yaş ve isim değişkenlerini oluşturalım
+yas = 14
+isim = "Emirhan baydar"
+
+# Kullanıcı bilgilerini ekrana yazdıralım
+# -> Adınız: Emirhan, Yaşınız: 14
+print("Adınız: " + isim + ", Yaşınız: " + str(yas))
+
+# karakter verisinin uzunluğuna bakalım
+print(len(isim))
+
+# str veri tipindeki değerin çeşitli elemanlarını yazdıralım
+# elemanlar 0'dan başlar
+print(isim[0])
+print(isim[1])
+print(isim[2])
+print(isim[3])

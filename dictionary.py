95b083f40399b958b90e5212a1bdba0b9c4d78c3
emrehanP <114801754+emrehanP@users.noreply.github.com>
2022-11-05 12:03:03 +0300

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -3,3 +3,50 @@ Dictionary "anahtar", "değer" ikililerinden oluşur
     "ad": "Eren"
     "soyad": "Özdal"
 """
+
+numaralar = [66, 75]
+isimler = ["ahmet", "mehmet"]
+
+numara = int(input("öğrenci numarasını yazınız : "))
+
+konum = numaralar.index(numara)
+print(isimler[konum])
+
+
+
+
+
+
+
+
+ogrenciler = {66: "ahmet", 75: "mehmet"}
+print(ogrenciler[numara])
+
+kisiler = {
+
+    1: {
+        "ad": "eren",
+        "soyad": "özdal",
+        "cinsiyet": True,
+        "dersler": ["türkçe"]
+    },
+    45: {
+        "ad": "eren",
+        "soyad": "özdal",
+        "cinsiyet": True,
+        "dersler": ["türkçe"]
+    }
+
+}
+
+print(kisiler[45]["dersler"])
+
+
+
+
+
+
+
+
+
+
